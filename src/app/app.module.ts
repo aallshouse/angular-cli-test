93173e57c7ceb76d417ed7484f8222ac72012406
Andrew Allshouse <andrew.allshouse@ddiworld.com>
2017-08-31 15:22:38 -0400

    Add new home-module with home component and use in the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeModuleModule } from './home-module/home-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
